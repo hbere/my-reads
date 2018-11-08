d21d876b5194d96be297a85de34c619cce5012f2
Hal Beresford <1526859+hbere@users.noreply.github.com>
2018-11-08 13:54:19 -0500

    Render entire author list for each book on Booklist

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -32,7 +32,9 @@ class ListBooks extends Component {
                                                         </div>
                                                     </div>
                                                     <div className="book-title">{book.title}</div>
-                                                    <div className="book-authors">{book.authors[0]}</div>
+                                                    {book.authors.map((author) => (
+                                                        <div className="book-authors">{author}</div>
+                                                    ))}
                                                 </div>
                                             </li>
                                         ))}
