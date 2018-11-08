925ceb648975b2bf95a5bd713e8e8874cdebcd89
Hal Beresford <1526859+hbere@users.noreply.github.com>
2018-11-08 11:17:58 -0500

    Delete unnecessary state tracking from App.js

--- src/App.js
+++ src/App.js
@@ -8,22 +8,16 @@ import './App.css'
 class BooksApp extends React.Component {
   state = {
     /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
+     * TODO consider whether need to track state (mutable info) here at all
      */
-    showSearchPage: false
   }
 
   render() {
     return (
       <div className="app">
-        {/* Formerly this.state.showSearchPage === true */}
         <Route exact path='/search' render={() => (
           <SearchBooks />
         )} />
-        {/* Formerly this.state.showSearchPage === false */}
         <Route exact path='/' render={() => (
           <Bookshelf />
         )} />
